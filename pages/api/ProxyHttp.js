3922f0e82b5ae512684c4d8f5da1bb3baec4a4a8
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2025-01-29 11:16:59 -0600

    Update ProxyHttp.js

--- pages/api/ProxyHttp.js
+++ pages/api/ProxyHttp.js
@@ -1,2 +1,20 @@
 export default async function handler(req, res){
+  let options = {
+    method: req.method,
+    headers: req.headers
+  }
+  if(req.headers["content-length"] && parseInt(req.headers["content-length"]) > 0){
+    options.body = req.body;
+  }
+  let responP = await fetch(process.env.URL, options);
+  responP.headers.forEach(function(val, key){
+    res.setHeader(key, val);
+  });
+  let stat = res.status(responP.status);
+  if(responP.headers["content-length"] && parseInt(responP.headers["content-length"]) > 0){
+    let resBody = await responP.arrayBuffer();
+    stat.send(Buffer.from(resBody));
+  }else{
+    stat.end();
+  }
 }
